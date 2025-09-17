b1f63bb42f27629fa6dfae0ed564cbb36e94f0a1
Shiv Gandhi <sgandhi2169@gmail.com>
2025-09-17 15:14:58 -0400

    added calendar and inbox page

---
+++ src/app/(Kambaz)/Calendar/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function Calendar() {
+    return (
+        <div>
+            <h2>Calendar</h2>
+            <h3>Coming Soon...</h3>
+        </div>
+    )
+}
---
+++ src/app/(Kambaz)/Inbox/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function Inbox() {
+    return (
+        <div>
+            <h2>Inbox</h2>
+            <h3>Coming Soon...</h3>
+        </div>
+    )
+}
