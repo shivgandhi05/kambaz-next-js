87d33014606902ff2f2afff3ec02884d16eae3f3
Shiv Gandhi <sgandhi2169@gmail.com>
2025-10-17 20:30:03 -0400

    made fixes again6

--- src/app/(Kambaz)/Courses/[cid]/People/Table/page.tsx
+++ src/app/(Kambaz)/Courses/[cid]/People/Table/page.tsx
@@ -6,40 +6,49 @@ import { Table } from "react-bootstrap";
 import { FaUserCircle } from "react-icons/fa";
 
 export default function PeopleTable() {
-    const { cid } = useParams();
-    const user = db.enrollments;
-    const enrollments = db.enrollments;
-    return (
-        <div id="wd-people-table" className="p-5">
-            <Table striped>
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Login ID</th>
-                        <th>Section</th>
-                        <th>Role</th>
-                        <th>Last Activity</th>
-                        <th>Total Activity</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {user.filter((usr) => enrollments.some((enrollment) => enrollment.user._id === usr._id && enrollment.course === cid)).map((user) => (
-                        <tr key={user._id}>
-                            <td className="wd-full-name text-nowrap">
-                            <FaUserCircle className="me-2 fs-1 text-secondary" />
-                            <span className="wd-first-name">{user.firstName}</span>{" "}
-                            <span className="wd-last-name">{user.lastName}</span>
-                            </td>
-                            <td className="wd-login-id">{user.loginId}</td>
-                            <td className="wd-section">{user.section}</td>
-                            <td className="wd-role">{user.role}</td>
-                            <td className="wd-last-activity">{user.lastActivity}</td>
-                            <td className="wd-total-activity">{user.totalActivity}</td>
+  const { cid } = useParams(); // get course ID from URL
+  const enrollments = db.enrollments; // each enrollment now includes a full user object
 
-                        </tr>
-                    ))}
-                </tbody>
-            </Table>
-        </div>
-    )
+  return (
+    <div id="wd-people-table" className="p-5">
+      <Table striped hover responsive>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Login ID</th>
+            <th>Section</th>
+            <th>Role</th>
+            <th>Last Activity</th>
+            <th>Total Activity</th>
+          </tr>
+        </thead>
+        <tbody>
+          {enrollments
+            .filter((enrollment: any) => enrollment.course === cid)
+            .map((enrollment: any) => (
+              <tr key={enrollment._id}>
+                <td className="wd-full-name text-nowrap">
+                  <FaUserCircle className="me-2 fs-1 text-secondary" />
+                  <span className="wd-first-name">
+                    {enrollment.user.firstName}
+                  </span>{" "}
+                  <span className="wd-last-name">
+                    {enrollment.user.lastName}
+                  </span>
+                </td>
+                <td className="wd-login-id">{enrollment.user.loginId}</td>
+                <td className="wd-section">{enrollment.user.section}</td>
+                <td className="wd-role">{enrollment.user.role}</td>
+                <td className="wd-last-activity">
+                  {enrollment.user.lastActivity}
+                </td>
+                <td className="wd-total-activity">
+                  {enrollment.user.totalActivity}
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </Table>
+    </div>
+  );
 }
