9288c6929e3156f5e631534158f3b189e90f5207
Shiv Gandhi <sgandhi2169@gmail.com>
2025-10-17 20:26:41 -0400

    made fixes again5"

--- src/app/(Kambaz)/Courses/[cid]/People/Table/page.tsx
+++ src/app/(Kambaz)/Courses/[cid]/People/Table/page.tsx
@@ -23,7 +23,7 @@ export default function PeopleTable() {
                     </tr>
                 </thead>
                 <tbody>
-                    {user.filter((usr) => enrollments.some((enrollment) => enrollment.user === usr._id && enrollment.course === cid)).map((user) => (
+                    {user.filter((usr) => enrollments.some((enrollment) => enrollment.user._id === usr._id && enrollment.course === cid)).map((user) => (
                         <tr key={user._id}>
                             <td className="wd-full-name text-nowrap">
                             <FaUserCircle className="me-2 fs-1 text-secondary" />
