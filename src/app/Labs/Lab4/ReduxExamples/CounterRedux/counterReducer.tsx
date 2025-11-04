5b87616fdbe58f29c424311fe89691b8c0c6c3a9
Shiv Gandhi <sgandhi2169@gmail.com>
2025-11-03 21:28:07 -0500

    fixed

--- src/app/Labs/Lab4/ReduxExamples/CounterRedux/counterReducer.tsx
+++ src/app/Labs/Lab4/ReduxExamples/CounterRedux/counterReducer.tsx
@@ -1,21 +1,21 @@
-"use client";
-import { createSlice } from "@reduxjs/toolkit";
+// "use client";
+// import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {
-    count: 0,
-};
-const counterSlice = createSlice({
-    name: "counter",
-    initialState,
-    reducers: {
-        increment: (state) => {
-            state.count = state.count + 1;
-        },
-        decrement: (state) => {
-            state.count = state.count - 1;
-        },
-    },
-});
+// const initialState = {
+//     count: 0,
+// };
+// const counterSlice = createSlice({
+//     name: "counter",
+//     initialState,
+//     reducers: {
+//         increment: (state) => {
+//             state.count = state.count + 1;
+//         },
+//         decrement: (state) => {
+//             state.count = state.count - 1;
+//         },
+//     },
+// });
 
-export const { increment, decrement } = counterSlice.actions;
-export default counterSlice.reducer;
+// export const { increment, decrement } = counterSlice.actions;
+// export default counterSlice.reducer;
