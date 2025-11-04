d479b5b670df53b0564da20e5a6dd21d9d756425
Shiv Gandhi <sgandhi2169@gmail.com>
2025-11-03 21:47:35 -0500

    fixed

--- src/app/Labs/Lab4/ReduxExamples/HelloRedux/index.tsx
+++ src/app/Labs/Lab4/ReduxExamples/HelloRedux/index.tsx
@@ -1,8 +1,9 @@
 "use client";
 import { useSelector, useDispatch } from "react-redux";
+import { RootState } from "../../store";
 export default function HelloRedux() {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const { message } = useSelector((state: any) => state.helloReducer);
+  const { message } = useSelector((state: RootState) => state.helloReducer);
   return (
     <div id="wd-hello-redux">
       <h3>Hello Redux</h3>
